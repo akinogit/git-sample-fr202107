8ab7644e3dae75ab6dc676a82f4123b6da623567
akinogit <akiwork7609@gmail.com>
2021-08-03 15:21:44 +0900

    初期追加のため

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from "axios";
 
 Vue.use(Vuex)
 
@@ -9,6 +10,21 @@ export default new Vuex.Store({
   mutations: {
   },
   actions: {
+    sample() {
+      axios
+        .get(
+          "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706",
+          {
+            params: {
+              applicationId: "1031636184299349316",
+              keyword: 'ビール',
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res)
+        });
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <input v-model="inputmozi" /><button @click="search">検索ボタン</button>
+    <div v-for="item in items" :key="item.itemCode">
+      <div>{{ item.itemName }}</div>
+      <span v-for="(gazo, index) in item.smallImageUrls" :key="index">
+        <img :src="gazo.imageUrl" />
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
+import { mapActions } from "vuex";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      items: [],
+      inputmozi: "",
+    };
+  },
+  methods: {
+    ...mapActions(["sample"]),
+    search() {
+      this.sample();
+      axios
+        .get(
+          "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706",
+          {
+            params: {
+              applicationId: "1031636184299349316",
+              keyword: this.inputmozi,
+            },
+          }
+        )
+        .then((res) => {
+          this.items = [];
+          console.log(res);
+          res.data.Items.forEach((element) => {
+            this.items.push(element.Item);
+          });
+        });
+    },
+  },
+};
 </script>
